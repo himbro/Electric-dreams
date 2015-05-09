f6319ade0767d4901f13e277b8e7f433650a1fd3
Alessio Bottosso <alessio.bottosso@gmail.com>
2015-05-09 10:44:34 +0200

    Networking API - New Scenes - Timer

---
+++ Electric Dreams/Assets/scripts/ShowDreams.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowDreams : MonoBehaviour {
+
+	public DreamList dreamList;
+
+	public Button previousDreamButton;
+	public Button nextDreamButton;
+
+	public Text dreamDescription;
+
+	public int currentDreamIndex = 0;
+
+	void Start () {
+		dreamList = GameObject.Find ("GameData").GetComponent<DreamList> ();
+
+		previousDreamButton.onClick.AddListener (delegate { Decrement(); });
+		nextDreamButton.onClick.AddListener (delegate { Increment(); }); 
+
+		dreamDescription.text = dreamList.player1Dreams [0];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		/* Abilita/Disabilita i pulsanti in base al sogno che sto mostrando */
+		if (currentDreamIndex == 0)
+			previousDreamButton.enabled = false;
+		else 
+			previousDreamButton.enabled = true;
+
+		if (currentDreamIndex == (dreamList.player1Dreams.Count - 1))
+			nextDreamButton.enabled = false;
+		else 
+			nextDreamButton.enabled = true;
+
+
+	}
+
+	void Increment () {
+		currentDreamIndex++;
+		dreamDescription.text = dreamList.player1Dreams [currentDreamIndex];
+	}
+
+	void Decrement () {
+		currentDreamIndex--;
+		dreamDescription.text = dreamList.player1Dreams [currentDreamIndex];
+	}
+}
---
+++ Electric Dreams/Assets/scripts/TimeManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/*
+ * Tale classe serve per contenere il tempo.
+ * */
+
+public class TimeManager : MonoBehaviour 
+{
+	/* we want a 3-minutes timer */
+	public int timeRemaining = 180; 
+
+	/* GUI values */
+	public int guiMinutes;
+	public int guiSeconds;
+	public string stringSeconds;
+
+	/* The Timer OnScreen */
+	public Text guiTimer;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		/* Decrease the timer by 1 second every seconds */
+		guiMinutes = 3;
+		guiSeconds = 0;
+		/* we want 2 decimals */
+		stringSeconds = "0" + guiSeconds.ToString ();
+		guiTimer.text = string.Format ("{0}:{1}", guiMinutes, stringSeconds);
+
+		InvokeRepeating ("DecreaseTimer", 1, 1);
+	}
+
+	void DecreaseTimer () {
+		guiTimer.text = string.Format ("{0}:{1}", guiMinutes, stringSeconds);
+		/* Decrease the remaining time */
+		timeRemaining -= 1;
+		/* Calculate the minutes for the UI */
+		guiMinutes = timeRemaining / 60;
+		/* Calculate the seconds for the UI */
+		guiSeconds = timeRemaining % 60;
+
+		if (guiSeconds >= 10) {
+			stringSeconds = guiSeconds.ToString();
+		}
+		else {
+			stringSeconds = "0" + guiSeconds.ToString();
+		}
+
+		/* Check if time elapsed */
+		if (timeRemaining < 0)
+			Invoke ("TimeElapsed", 1);
+	}
+
+	void TimeElapsed () {
+		Application.LoadLevel (5);
+	}
+}
