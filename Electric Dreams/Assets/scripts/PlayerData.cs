3a14a1fbca60923d5589ca7796470aae845c3085
Alessio Bottosso <alessio.bottosso@gmail.com>
2015-05-12 17:42:34 +0200

    First network attempts + new scenes

---
+++ Electric Dreams/Assets/scripts/PlayerData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerData : MonoBehaviour {
+
+	public string playerName;
+
+	public Text nameField;
+	
+	void Awake () {
+		DontDestroyOnLoad (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setName () {
+		if (nameField.text.Length != 0) {
+			this.playerName = nameField.text.ToString ();
+		}	
+		else {
+			this.playerName = "Guest" + Random.Range(1, 9999);
+		}
+	}
+}
---
+++ Electric Dreams/Assets/scripts/WaitPlayers.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaitPlayers : MonoBehaviour {
+	
+	public Text[] playerList = new Text[6];
+
+	public Button startButton;
+
+	public void Awake()
+	{
+		// in case we started this demo with the wrong scene being active, simply load the menu scene
+		if (!PhotonNetwork.connected)
+		{
+			Application.LoadLevel(2);
+			return;
+		}
+		
+		// we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+		//PhotonNetwork.Instantiate(this.playerPrefab.name, transform.position, Quaternion.identity, 0);
+	
+		int i = 0;
+
+		foreach (PhotonPlayer player in PhotonNetwork.playerList) {
+			playerList[i].text = player.name;
+			i++;
+		}
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PhotonNetwork.playerList.Length == 2)
+			startButton.enabled = true;
+		Debug.Log (PhotonNetwork.playerList.Length);
+	}
+}
